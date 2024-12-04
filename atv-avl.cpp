7b8fafc2db5c73ce32fd0c52fdcebaf10cb8f45e
JandersonLSilva <janderson.user@gmail.com>
2024-12-04 19:01:02 -0300

    Terceira versão do algoritmo: implementado a lógica do balanço de cada nó e parcialmente implementado as funções que fazem o balanceamento da árvore, conforme os nós ficam desbalanceados.

--- atv-avl.cpp
+++ atv-avl.cpp
@@ -38,32 +38,102 @@ Tree* create_node(int v){
     return node;
 }
 
-void insert_node_Tree(Tree *tree, Tree *node){
-    if(node->data < tree->data){
-        // if(tree->bal == 0){
-        //     h = true;
-        // }
-        // else{
-        //     h = false;
-        // }
-        // if(h == true) tree->bal--;
-
-        if(tree->left == NULL) {
-            tree->left = node;
-        }
-        else {
-            insert_node_Tree(tree->left, node);
-        }
+// Função que faz a rotação simples
+void simple_rotation(Tree* t){
+    if(tree->bal ==  -2 && tree->left->bal == -1){ // se o balanço da árvore atual for -2 e o balanço do nó a esquerda for -1
+        Tree* node_p = t; // guardando a árvore atual
+        Tree* node_u = t->left; // guardando o nó a esquerda da árvore atual
 
     }
+    else if(tree->bal ==  2 && tree->right->bal == 1){ // se o balanço da árvore atual for 2 e o balanço do nó a direita for 1
+        Tree* node_p = t; // guardando a árvore atual
+        Tree* node_z = t->right; // guardando o nó a direita da árvore atual
+    }
+}
+
+// Função que faz a rotação dupla da árvore
+void double_rotaation(Tree* t){
+    if(tree->bal ==  -2 && tree->left->bal == 1){ // se o balanço da árvore atual for -2 e o balanço do nó a esquerda for 1
+        Tree* node_p = t; // guardando a árvore atual
+        Tree* node_u = t->left; // guardando o nó a esquerda da árvore atual
+        Tree* node_v = t->left->right; // guardando o nó a direita do nó a esquerda da árvore atual
+    }
+    else if(tree->bal ==  2 && tree->right->bal == -1){ // se o balanço da árvore atual for 2 e o balanço do nó a direita for -1
+        Tree* node_p = t; // guardando a árvore atual
+        Tree* node_z = t->right; // guardando o nó a direita da árvore atual
+        Tree* node_y = t->right->left; // guardando o nó a esquerda do nó a direita da árvore atual
+    }
+}
+
+
+// Função que inseri um nó ná arvore
+void insert_node_Tree(Tree *tree, Tree *node){
+    int bal_temp; // declara uma de balanceamento temporária
+
+    // verificando se o valor do nó a ser inserido é menor que o valor do nó em questão
+    if(node->data < tree->data){
+
+        // verificando se o prŕoximo nó a esquerda da arvore atual está vazia
+        if(tree->left == NULL) {
+            tree->left = node; // insere o nó a esquerda da árvore, já que o nó está vazio
+
+            tree->bal--; // diminui o balanceamento, já que o nó foi inserido a esquerda
+
+            return; // finaliza a chamada atual da função
+        }
+        else { // se o nó a esquerda não estiver vazio
+            bal_temp = tree->left->bal; // guarda o balanceamento atual do nó em questão
+
+            insert_node_Tree(tree->left, node);
+            // realiza a chamada da própia função, passando o nó a esquerda que ainda precisa ser comparada com o nó em questão
+
+            if((bal_temp == 0) && (tree->left->bal != 0)){
+                // se o balanceamento do nó esquerdo da arvore atual era 0 e passou a ser 1 ou -1, então continua a diminuir o balanceamento
+
+                tree->bal--; // efetiva a diminuição
+
+                if(tree->bal ==  -2 && tree->left->bal == -1){ // se o balanço da árvore atual for -2 e o balanço do nó a esquerda for -1
+                    simple_rotation(tree); // chama a função de rotação simples
+                }
+                else if(tree->bal ==  -2 && tree->left->bal == 1){ // se o balanço da árvore atual for -2 e o balanço do nó a esquerda for 1
+                    double_rotation(tree); // chama a função de rotação simples
+                }
+            }
+            return; // finaliza a chamada atual da função
+        }
+    }
+
+    // verificando se o valor do nó a ser inserido é maior que o valor do nó em questão
     if(node->data > tree->data){
+        // verificando se o prŕoximo nó a direita da arvore atual está vazia
         if(tree->right == NULL) {
-            tree->right = node;
+            tree->right = node; // insere o nó a direita da árvore, já que o nó está vazio
+
+            tree->bal++;// aumenta o balanceamento, já que o nó foi inserido a direita
+
+            return; // finaliza a chamada atual da função
         }
-        else {
+        else { // se o nó a esquerda não estiver vazio
+            bal_temp = tree->right->bal; // guarda o balanceamento atual do nó em questão
+
             insert_node_Tree(tree->right, node);
+            // realiza a chamada da própia função, passando o nó a direita que ainda precisa ser comparada com o nó em questão
+
+            if((bal_temp == 0) && (tree->right->bal != 0)){
+                // se o balanceamento do nó direito da arvore atual era 0 e passou a ser 1 ou -1, então continua a aumentar o balanceamento
+
+                tree->bal++; // efetiva o aumento
+
+                if(tree->bal ==  2 && tree->right->bal == 1){ // se o balanço da árvore atual for 2 e o balanço do nó a direita for 1
+                    simple_rotation(tree); // chama a função de rotação simples
+                }
+                else if(tree->bal ==  2 && tree->right->bal == -1){ // se o balanço da árvore atual for 2 e o balanço do nó a direita for -1
+                    double_rotation(tree); // chama a função de rotação dupla
+                }
+            }
+            return; // finaliza a chamada atual da função
         }
-        tree->bal++;
+
 
     }
 }
